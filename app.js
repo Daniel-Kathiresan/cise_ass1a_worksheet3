b760c1c303d229cb684848f882e8d16014eeead8
DanielK2001 <daniel.kathiresan@gmail.com>
2022-04-01 13:44:09 +1300

    First Commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
+require("dotenv").config();
 
 // routes
 const books = require('./routes/api/books');
@@ -16,13 +17,23 @@ connectDB();
 app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+// app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
